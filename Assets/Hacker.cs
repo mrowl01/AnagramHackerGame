b67b9311e7573b063390601499a93fdca4313fd2
Chris <christopherortega56@gmail.com>
2017-11-13 23:55:07 -0600

    Refactor and adding variable for a greeting

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
+	string greeting = "Hey Hacka";
 	// Use this for initialization
 	void Start () {
-		Terminal.WriteLine ("What would you like to hack into?"); 
-		Terminal.WriteLine ("Enter 1 : Library"); 
-		Terminal.WriteLine ("Enter 2 : Police Station"); 
-		Terminal.WriteLine ("Enter your selection:"); 
+		ShowMainMenu (); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void ShowMainMenu()
+	{
+		Terminal.ClearScreen (); 
+		Terminal.WriteLine ("What would you like to hack into?"); 
+		Terminal.WriteLine ("Enter 1 : Library"); 
+		Terminal.WriteLine ("Enter 2 : Police Station"); 
+		Terminal.WriteLine ("Enter your selection:"); 
 	}
 }
