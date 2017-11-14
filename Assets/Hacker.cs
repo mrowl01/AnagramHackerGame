72f5f108256b10c8ba2e4a8f190ca1c4127aa63c
Chris <christopherortega56@gmail.com>
2017-11-13 23:41:05 -0600

    Setting up game

---
+++ Assets/Hacker.cs
@@ -0,0 +1,19 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Terminal.WriteLine ("What would you like to hack into?"); 
+		Terminal.WriteLine ("Enter 1 : Library"); 
+		Terminal.WriteLine ("Enter 2 : Police Station"); 
+		Terminal.WriteLine ("Enter your selection:"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
