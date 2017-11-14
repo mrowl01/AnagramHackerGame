8e5ee58d45cf6af0ac0cfd3f5f876f0543e3c29a
Chris <christopherortega56@gmail.com>
2017-11-14 02:10:58 -0600

    Just changes relevant to courses reviewing state changes and variables

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
-
-	string greeting = "Hey Hacka";
+	int level;
 	// Use this for initialization
 	void Start () {
-		ShowMainMenu (); 
+		
+		ShowMainMenu ("Hello Hacka"); 
+
 	}
 	
 	// Update is called once per frame
@@ -15,12 +16,36 @@ public class Hacker : MonoBehaviour {
 		
 	}
 
-	void ShowMainMenu()
+	void ShowMainMenu(string greeting)
 	{
 		Terminal.ClearScreen (); 
+		Terminal.WriteLine (greeting); 
 		Terminal.WriteLine ("What would you like to hack into?"); 
 		Terminal.WriteLine ("Enter 1 : Library"); 
 		Terminal.WriteLine ("Enter 2 : Police Station"); 
 		Terminal.WriteLine ("Enter your selection:"); 
 	}
+	void OnUserInput (string theInput)
+	{
+		if (theInput == "1" || theInput == "2" || theInput == "3") {
+			int tempInt;
+			tempInt= int.Parse (theInput);
+			level = tempInt; 
+			StartGame ();
+
+		} else if (theInput == "007") {
+			Terminal.WriteLine ("Hello, Mr.Bond");
+		} else if (theInput == "menu") {
+			ShowMainMenu ("Hello Hacker"); 
+		}
+		else {
+			Terminal.WriteLine ("Not a valid option"); 
+		}
+	}
+	void StartGame ( )
+	{
+		Terminal.WriteLine ("You have choesen level " + level); 
+	}
+
+
 }
